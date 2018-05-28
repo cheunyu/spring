059c041cc817063cb7d47fb6c989d08aafecf6f5
cheunyu <cheunyufoo@gmail.com>
2018-05-29 00:37:32 +0800

    AOP StaticMethodMatcherPointcutAdvisor

---
+++ src/main/java/com/aop/advisor/staticmethodmatcher/GreetingAdvisor.java
@@ -0,0 +1,26 @@
+package com.aop.advisor.staticmethodmatcher;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author:cheunyu
+ * @date:2018/5/28 19:46
+ */
+public class GreetingAdvisor extends StaticMethodMatcherPointcutAdvisor {
+    @Override
+    public boolean matches(Method method, Class<?> aClass) {
+        return "greetTo".equals(method.getName());
+    }
+
+    public ClassFilter getClassFilter() {
+        return new ClassFilter() {
+            @Override
+            public boolean matches(Class<?> aClass) {
+                return Waiter.class.isAssignableFrom(aClass);
+            }
+        };
+    }
+}
