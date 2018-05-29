6b5a63dbe0ea38c3de0ccb4be8eac8317b0d4aaa
cheunyu <cheunyufoo@gmail.com>
2018-05-29 19:56:33 +0800

    AOP DefaultAdvisorAutoProxyCreator

---
+++ src/main/java/com/aop/autoproxy/defaultadvisorautoproxycreator/Seller.java
@@ -0,0 +1,12 @@
+package com.aop.autoproxy.defaultadvisorautoproxycreator;
+
+/**
+ * @author:cheunyu
+ * @date:2018/5/28 19:45
+ */
+public class Seller {
+
+    public void greetTo(String name) {
+        System.out.println("seller greet to " + name + "...");
+    }
+}
---
+++ src/main/java/com/aop/autoproxy/defaultadvisorautoproxycreator/Waiter.java
@@ -0,0 +1,16 @@
+package com.aop.autoproxy.defaultadvisorautoproxycreator;
+
+/**
+ * @author:cheunyu
+ * @date:2018/5/28 19:44
+ */
+public class Waiter {
+
+    public void greetTo(String name) {
+        System.out.println("greet to " + name + "...");
+    }
+
+    public void serveTo(String name) {
+        System.out.println("serving " + name + "...");
+    }
+}
