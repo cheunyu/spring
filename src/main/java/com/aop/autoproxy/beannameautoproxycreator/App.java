9cb439463515d322dca278a625afc243ea9d1eca
cheunyu <cheunyufoo@gmail.com>
2018-05-29 18:31:06 +0800

    AOP BeanNameAutoProxyCreator

---
+++ src/main/java/com/aop/autoproxy/beannameautoproxycreator/App.java
@@ -0,0 +1,23 @@
+package com.aop.autoproxy.beannameautoproxycreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author:cheunyu
+ * @date:2018/5/28 16:13
+ */
+public class App {
+
+    @Test
+    public void before() {
+        String xmlPath = "com/aop/autoproxy/beannameautoproxycreator/applicationContext.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(xmlPath);
+        Waiter waiter = (Waiter) ctx.getBean("waiterTarget");
+        Seller seller = (Seller) ctx.getBean("sellerTarget");
+        waiter.greetTo("Panda");
+        waiter.serveTo("Panda");
+        seller.greetTo("Panda");
+    }
+}
